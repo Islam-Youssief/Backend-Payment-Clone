8ba26e3c179d3ee25f48fc9cbee6e713c3d53ed3
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-23 14:49:34 +0300

    add base url && make env private

--- api/services/payment/checkout.py
+++ api/services/payment/checkout.py
@@ -3,15 +3,17 @@ import os
 import api.services.requests_service as requests_service
 
 
+CHECKOUT_PAYMENT_BASE_URL='https://sl73izp6.api.sandbox.checkout.com'
+CHECKOUT_PAYMENT_URL=CHECKOUT_PAYMENT_BASE_URL + '/payments'
 
 
 class CheckoutClient:
     def __init__(self , env , test_request_sender=None):
-        self.env = env 
+        self._env = env 
         self._request_sender = test_request_sender or requests_service.get_service(env)
     
     def pay_with_card(self, data):
-        response = self._request_sender.request(method='POST', url='/payments', data=data, headers=self._headers)
+        response = self._request_sender.request(method='POST', url=CHECKOUT_PAYMENT_URL, data=data, headers=self._headers)
         return response
 
     @property
