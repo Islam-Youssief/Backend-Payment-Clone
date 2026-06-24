39561f983a67686ea500682159ea22c27358be97
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-24 18:12:29 +0300

    fix: move wiremock config to AppConfig

--- api/core/configurations.py
+++ api/core/configurations.py
@@ -6,21 +6,14 @@ class AppConfig:
     @property
     def env(self):
         return os.environ.get('ENV', 'lcl')
-<<<<<<< HEAD
 
     @property
     def wiremock_url(self):
         return f'http://127.0.0.1:{self.wiremock_port}'
-
+        
     @property
     def wiremock_port(self):
         return '8080'
-
-class PaymentsConfig:
-    @property
-    def stripe_secret_key(self):
-        return os.environ.get('STRIPE_SECRET_KEY')
-=======
     
 class PaymentConfig:
 
@@ -31,4 +24,4 @@ class PaymentConfig:
     @property
     def checkout_secret_key(self):
         return os.environ.get('CHECKOUT_SECRET_KEY')
->>>>>>> 7a31ed1 (feat: add payment config)
+
