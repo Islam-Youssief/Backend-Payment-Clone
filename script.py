45c86402115f1934176331b281b575f5c27a9d10
mohamedriyad1 <mohamedelhenawy2012yahoo.comgit config --global user.email mohamedelhenawy2012yahoo.com>
2026-06-25 11:29:57 +0300

    feat:  Add fawry swagger and wiremock mapping

--- script.py
+++ script.py
@@ -1,14 +1,21 @@
 
 import api.services.payment.paypal as paypal
+import api.services.payment.fawry as fawry
 
 
 client = paypal.PayPalClient('lcl')
 response = client.pay_with_visa(data={"amount": 10.00, "currency": "EGP", "payment_method": "visa"})
 print(response.json())
 
-
-
-
-
-
-
+client = fawry.FawryClient('lcl')
+response = client.pay_with_visa(data={
+    "merchantRefNum": "ORDER_123456",
+    "customerProfileId": "CUST_987",
+    "amount": 100.50,
+    "cardNumber": "4111111111111111",
+    "cardExpiryYear": "25",
+    "cardExpiryMonth": "12",
+    "cvv": "123",
+    "signature": "mock_signature_hash"
+  })
+print(response.json())
