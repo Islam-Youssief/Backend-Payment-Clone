129bb978cb9993a179456334a3a30eea03a8e7cd
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-29 16:03:03 +0300

    feat:  Add fawry swagger and wiremock mapping

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
-
-import api.services.payment.stripe as stripe
-import api.services.payment.paypal as paypal
 import api.services.payment.checkout as checkout
+import api.services.payment.fawry as fawry
+import api.services.payment.paypal as paypal
+import api.services.payment.stripe as stripe
+
 
 client = paypal.PayPalClient('lcl')
 response = client.pay_with_visa(data={"amount": 10.00, "currency": "EGP", "payment_method": "visa"})
@@ -19,14 +20,23 @@ response_client = checkout_client.pay_with_card(data={"source":{
         "amount": 1000,
         "processing_channel_id": "pc_test_123"})
 
+print(response_client.json())
+
+
 client = stripe.StripeClient('lcl')
 response = client.create_payment_intent(data={"user_name": "Ahmed Mohammad Ali", "amount": "100", "visa_number": "4242424242424242","cvv":"123"})
 print(response.json())
 
 
-
-
-
-
-print(response_client.json())
-
+client = fawry.FawryClient('lcl')
+response = client.pay_with_visa(data={
+    "merchantRefNum": "ORDER_123456",
+    "customerProfileId": "CUST_987",
+    "amount": 100.50,
+    "cardNumber": "4111111111111111",
+    "cardExpiryYear": "25",
+    "cardExpiryMonth": "12",
+    "cvv": "123",
+    "signature": "mock_signature_hash"
+  })
+print(response.json())
