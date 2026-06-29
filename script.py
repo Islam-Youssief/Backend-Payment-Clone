5590bd686961b031254d0e0592e4827133b368f2
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-29 16:10:33 +0300

    feat: configure WireMock for Checkout APIs

--- script.py
+++ script.py
@@ -8,25 +8,6 @@ client = paypal.PayPalClient('lcl')
 response = client.pay_with_visa(data={"amount": 10.00, "currency": "EGP", "payment_method": "visa"})
 print(response.json())
 
-checkout_client = checkout.CheckoutClient('lcl')
-response_client = checkout_client.pay_with_card(data={"source":{
-            "type": "card",
-            "number": "2242424242424242",
-            "cvv": "100",
-            "expiry_month": 12,
-            "expiry_year": 2030
-        },
-        "currency": "USD",
-        "amount": 1000,
-        "processing_channel_id": "pc_test_123"})
-
-print(response_client.json())
-
-
-client = stripe.StripeClient('lcl')
-response = client.create_payment_intent(data={"user_name": "Ahmed Mohammad Ali", "amount": "100", "visa_number": "4242424242424242","cvv":"123"})
-print(response.json())
-
 
 client = fawry.FawryClient('lcl')
 response = client.pay_with_visa(data={
@@ -40,3 +21,20 @@ response = client.pay_with_visa(data={
     "signature": "mock_signature_hash"
   })
 print(response.json())
+
+client = stripe.StripeClient('lcl')
+response = client.create_payment_intent(data={"user_name": "Ahmed Mohammad Ali", "amount": "100", "visa_number": "4242424242424242","cvv":"123"})
+print(response.json())
+
+checkout_client = checkout.CheckoutClient('lcl')
+response_client = checkout_client.pay_with_card(data={"source":{
+            "type": "card",
+            "number": "2242424242424242",
+            "cvv": "100",
+            "expiry_month": 12,
+            "expiry_year": 2030
+        },
+        "currency": "USD",
+        "amount": 1000,
+        "processing_channel_id": "pc_test_123"})
+print(response_client.json())
