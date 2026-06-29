859b928f7a92b816cd040d00cc9e72a4d36c4eea
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-29 18:06:30 +0300

    refactor: payment url

--- api/services/payment/checkout.py
+++ api/services/payment/checkout.py
@@ -6,8 +6,7 @@ import api.services.requests_service as requests_service
 import api.core.exceptions as exception
 
 
-CHECKOUT_PAYMENT_BASE_URL=f'https://{configuration.PaymentsConfig().checkout_prefix_code}.api.sandbox.checkout.com'
-CHECKOUT_PAYMENT_URL=f'{CHECKOUT_PAYMENT_BASE_URL}/payments'
+CHECKOUT_PAYMENT_URL = "https://api.sandbox.checkout.com/payments"
 
 
 class CheckoutClient:
@@ -19,7 +18,7 @@ class CheckoutClient:
     
     def pay_with_card(self, data):
         self._user_data_validator.validate_payment_data(data)
-        response = self._request_sender.request(method='POST', url=CHECKOUT_PAYMENT_URL, data=json.dumps(data), headers=self._headers)
+        response = self._request_sender.request(method='POST', url=f'{configuration.PaymentsConfig().checkout_prefix_code}.{CHECKOUT_PAYMENT_URL}', data=json.dumps(data), headers=self._headers)
         return response
 
     @property
