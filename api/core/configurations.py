76d5a080c5ad42f2fa2c51926f582ea43952d188
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-29 16:05:47 +0300

    fix: move wiremock config to AppConfig

--- api/core/configurations.py
+++ api/core/configurations.py
@@ -25,6 +25,10 @@ class PaymentsConfig:
         return os.environ.get('FAWRY_SECURITY_KEY')
 
     @property
+    def stripe_secret_key(self):
+        return os.environ.get('STRIPE_SECRET_KEY')
+
+    @property
     def checkout_prefix_code(self):
         return os.environ.get('CHECKOUT_PREFIX_CODE')
 
