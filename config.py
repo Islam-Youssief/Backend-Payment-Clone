5ca9843110f120878d0a07e8644d5f07b344328d
manar bshr <manarbshr84@gmail.com>
2026-07-05 03:18:00 +0300

    "refactor: remove fake gateway implementation and unused files for mezza"

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///payments.db")
-    DEBUG = True
