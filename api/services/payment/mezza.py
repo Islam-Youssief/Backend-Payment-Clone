8b1f2bbc36087d717d732b9448e750a3e0ed2863
manar bshr <manarbshr84@gmail.com>
2026-07-05 05:28:56 +0300

    refactor:remove fake getway and Implement meeza  client&auth builder

--- api/services/payment/mezza.py
+++ api/services/payment/mezza.py
@@ -1,66 +1,67 @@
-import time
-import uuid
-
 import api.services.requests_service as requests_service
+import api.services.payment.mezza_validator as validators
+import api.core.configurations as configurations
+import time
+import hashlib
 
 
-class MezzaPaymentService:
-    def __init__(self, env, test_request_sender=None):
+MEZZA_PAYMENT_URL = "https://api.dlocal.com/payments"
+
+
+class MezzaClient:
+
+    def __init__(self, env, test_request_sender=None, test_validator=None, test_headers_builder=None):
         self._env = env
         self._request_sender = test_request_sender or requests_service.get_service(self._env)
-        self._payments = {}
+        self._validator = test_validator or validators.MeezaPaymentDataValidator()
+        self._headers_builder = test_headers_builder or _MeezaAuthBuilder()
 
     def create_payment(self, data):
-        payment_id = f'R-{uuid.uuid4()}'
-        payment = {
-            'id': payment_id,
-            'amount': data['amount'],
-            'currency': data['currency'],
-            'country': data['country'],
-            'payment_method_id': data['payment_method_id'],
-            'payment_method_type': 'BANK_TRANSFER',
-            'payment_method_flow': data['payment_method_flow'],
-            'order_id': data['order_id'],
-            'description': data['description'],
-            'payer': data['payer'],
-            'notification_url': data['notification_url'],
-            'callback_url': data['callback_url'],
-            'created_date': time.strftime('%Y-%m-%dT%H:%M:%S.000+0000'),
-            'status': 'PENDING',
-            'status_detail': 'The payment is pending.',
-            'status_code': '100',
-            'redirect_url': f'http://127.0.0.1:5000/mock/pay/{payment_id}',
+        self._validator.validate(data)
+
+        response = self._request_sender.request(
+            method='POST',
+            url=MEZZA_PAYMENT_URL,
+            json=data,
+            headers=self._headers_builder.build_headers(data)
+        )
+                
+
+        return response
+
+    @property
+    def _headers(self):
+        return {
+            'Content-Type': 'application/json'
         }
-        self._payments[payment_id] = payment
-        return payment
+    
 
-    def get_payment(self, payment_id):
-        return self._payments.get(payment_id)
 
-    def mark_paid(self, payment_id):
-        payment = self._payments.get(payment_id)
-        if payment:
-            payment['status'] = 'PAID'
-            payment['status_code'] = '200'
-            payment['status_detail'] = 'The payment was paid.'
-        return payment
 
-    def mark_failed(self, payment_id):
-        payment = self._payments.get(payment_id)
-        if payment:
-            payment['status'] = 'FAILED'
-            payment['status_code'] = '400'
-            payment['status_detail'] = 'Payment failed.'
-        return payment
 
-    def send_webhook(self, payment):
-        try:
-            self._request_sender.request(method='POST', url=payment['notification_url'], json=payment)
-        except Exception:
-            pass  # ponytail: best-effort webhook; merchant outage must not fail the gateway
+class _MeezaAuthBuilder:
 
-    def async_finalize(self, payment_id):
-        time.sleep(5)
-        payment = self.mark_paid(payment_id)
-        if payment:
-            self.send_webhook(payment)
+    def __init__(self):
+        self._config = configurations.PaymentsConfig()
+
+    def build_headers(self, data=None):
+        return {
+            "X-Login": self._config.meeza_login,
+            "X-Trans-Key": self._config.meeza_trans_key,
+            "X-Date": str(int(time.time())),
+            "Content-Type": "application/json",
+            "Authorization": self._build_signature(data),
+        }
+
+    def _build_signature(self, data):
+        """
+        Mock signature (NOT real dLocal implementation)
+        just to simulate Fawry-like behavior
+        """
+        base_string = (
+            f"{self._config.meeza_login}"
+            f"{self._config.meeza_trans_key}"
+            f"{data.get('amount', '') if data else ''}"
+        )
+
+        return hashlib.sha256(base_string.encode()).hexdigest()
