f911e4a672097304661c681e6527ac27740164c0
Exil <abdalrahmankay2019@gmail.com>
2026-07-06 12:19:52 +0300

    Add Paymob implementation

---
+++ api/services/payment/paymob.py
@@ -0,0 +1,85 @@
+import api.services.requests_service as requests_service
+import api.core.configurations as config
+import api.core.exceptions as exceptions
+PAYMOB_INTENTION_URL = "https://accept.paymob.com/v1/intention/"
+
+class PaymobClient:
+    def __init__(self,env,test_request_sender = None,test_validator = None):
+       self._env = env
+       self._request_sender = test_request_sender or requests_service.get_service(self._env)
+       self._validator = test_validator  or PaymobUserDataValidator() 
+       self._secret_key = config.PaymentsConfig.paymob_secret_key 
+
+    def _build_headers(self):
+        return {
+            "Authorization" : f"Token {self._secret_key}",
+            'Content-Type' : 'application/json'
+            }
+    
+    def create_intention(self,data):
+        self._validator.validate(data)
+        response = self._request_sender.request(
+            method= "POST",
+            url= PAYMOB_INTENTION_URL,
+            headers= self._build_headers(),
+            json= data  
+        )
+        return response
+
+class PaymobUserDataValidator:
+    def validate(self,data):
+        required_fields = ["amount","currency","payment_methods","items","billing_data"]
+        missing_fields = [field for field in required_fields if field not in data]
+        if missing_fields:
+            raise exceptions.RequiredInputError(f'Missing fields: {missing_fields}')
+        self._validate_amount(data['amount'])
+        self._validate_currency(data['currency'])
+        self._validate_payment_methods(data['payment_methods'])
+        self._validate_items(data['items'])
+        self._validate_total_amount(data['amount'],data['items'])
+        self._validate_billing_data(data['billing_data'])
+
+    def _validate_amount(self,amount):
+        if not isinstance(amount,int):
+            raise exceptions.InputDataTypeError("Amount must be an integar representing amount in cents")
+        if amount <= 0:
+            raise exceptions.ValidationError("Amount must be higher than zero")
+    
+    def _validate_items(self,items):
+        if not isinstance(items,list):
+            raise exceptions.InputDataTypeError(f"Expected list")
+        if len(items) == 0:
+            raise exceptions.RequiredInputError(f"Missing items")
+        for item in items:
+            if item.get("name") is None:
+                raise exceptions.RequiredInputError(f"Missing item name")
+            if item.get("amount") is None:
+                raise exceptions.RequiredInputError(f"Missing item amount")
+
+    def _validate_total_amount(self,amount,items):
+        _total_amount = 0
+        for item in items:
+            _total_amount +=item["amount"]
+        if amount !=_total_amount:
+            raise exceptions.ValidationError(f"Amount doesn't match")
+        
+    def _validate_currency(self,currency):
+       if not isinstance(currency, str):
+            raise exceptions.InputDataTypeError(f"Expected string") 
+
+    def _validate_billing_data(self,billing_data):
+        required_fields = ["first_name","last_name","email"]
+        missing_fields = [field for field in required_fields if not billing_data.get(field)]
+        if missing_fields:
+            raise exceptions.RequiredInputError(f'Missing fields: {missing_fields}')
+            
+    def _validate_payment_methods(self,payment_methods):
+         if not isinstance(payment_methods, list):
+            raise exceptions.InputDataTypeError(f"Expected list")
+         if len(payment_methods) == 0:
+            raise exceptions.RequiredInputError("Payment Methods can't be empty")
+         for method in payment_methods:
+            if not isinstance(method,int):
+                raise exceptions.ValidationError("Integration ID is required")
+            
+    
---
+++ tests/test_api/test_services/test_payment/test_paymob.py
@@ -0,0 +1,121 @@
+import unittest
+import api.services.payment.paymob as paymob
+import api.services.requests_service as requests_service
+from assertpy import assert_that
+import tests.doubles.requests as requests_doubles
+import api.core.exceptions as exceptions
+
+class TestPaymobClient(unittest.TestCase):
+    def setUp(self):
+        self.request_sender = requests_doubles.RequestSenderDouble(json={"message" : "success"})
+        self.validator = _UserDataValidatorDouble()
+        self.client = paymob.PaymobClient('lcl',self.request_sender,self.validator)
+        self.fake_data={
+                    "amount": 2000,
+                    "currency": "EGP",
+                    "payment_methods": [158],
+                    "items": [
+                        {
+                        "name": "Item name",
+                        "amount": 2000,
+                        "description": "Item description",
+                        "quantity": 1
+                        }],
+                    "billing_data": {
+                        "apartment": "dumy",
+                        "first_name": "ala",
+                        "last_name": "zain",
+                        "street": "dumy",
+                        "building": "dumy",
+                        "phone_number": "+92345xxxxxxxx",
+                        "city": "dumy",
+                        "country": "dumy",
+                        "email": "ali@gmail.com",
+                        "floor": "dumy",
+                        "state": "dumy"
+                    },
+                    "extras": {
+                        "ee": 22
+                    },
+                    "special_reference": "phe4sjw11q-1xxxxxxxxx",
+                    "expiration": 3600,
+                    "notification_url": "https://webhook.site/dabe4968-5xxxxxxxxxxxxxxxxxxxxxx",
+                    "redirection_url": "https://www.google.com/"
+                    }
+        
+    def test_client_uses_real_if_doubles_are_not_sent(self):
+        client_without_doubles  = paymob.PaymobClient('prd')
+        assert_that(client_without_doubles._request_sender).is_instance_of(requests_service.RequestsWrapper)
+        assert_that(client_without_doubles._validator).is_instance_of(paymob.PaymobUserDataValidator)
+
+    def test_client_create_intention_calls_requests_with_expected_params(self):
+        response = self.client.create_intention(self.fake_data)
+        assert_that(response.json().get('message')).is_equal_to('success')
+        self.request_sender.assert_that_request_is_called_with(
+           method= "POST",
+            url= paymob.PAYMOB_INTENTION_URL,
+            headers= {
+                     "Authorization": f"Token {self.client._secret_key}",
+                     "Content-Type": "application/json"
+                    },
+            json= self.fake_data
+        )
+        self.validator.assert_that_validation_is_called_with(self.fake_data)
+
+
+class TestUserDataValidator(unittest.TestCase):
+    def setUp(self):
+        self.validator = paymob.PaymobUserDataValidator()
+        self.fake_data ={
+            "amount": 2000,
+            "currency": "EGP",
+            "payment_methods": [158],
+            "items": [
+                {
+                "name": "Item name",
+                "amount": 2000,
+                }],
+            "billing_data": {
+                "first_name": "ala",
+                "last_name": "zain",
+                "email": "ali@gmail.com",
+            }}
+    def test_raises_required_input_error_when_field_are_missing(self):
+        with self.assertRaises(exceptions.RequiredInputError):
+            self.validator.validate(data={'amount':34})
+
+    def test_raises_validation_error_when_amount_is_zero(self):#
+        invalid_data = {**self.fake_data, 'amount': 0}
+        with self.assertRaises(exceptions.ValidationError):
+            self.validator.validate(data=invalid_data)
+
+    def test_raises_validation_error_when_amount_is_negative(self):
+        invalid_data = {**self.fake_data,'amount' : -69}
+        with self.assertRaises(exceptions.ValidationError):
+            self.validator.validate(data=invalid_data)
+    
+    def test_raises_input_data_type_error_when_amount_is_string(self):
+        invalid_data = {**self.fake_data, 'amount' : 'xD20'}
+        with self.assertRaises(exceptions.InputDataTypeError):
+            self.validator.validate(data=invalid_data)
+    
+    def test_raises_required_input_error_when_email_is_missing(self):
+        invalid_data = {**self.fake_data,"billing_data":{"first_name" : "ala","last_name" : "zain"}}
+        with self.assertRaises(exceptions.RequiredInputError):
+            self.validator.validate(invalid_data)
+            
+    def test_valid_data_passes(self):
+            self.validator.validate(self.fake_data)
+
+class  _UserDataValidatorDouble:
+        def __init__(self):
+            self._validate_called_with = None
+
+        def validate(self, data):
+            self._validate_called_with = data
+
+        def assert_that_validation_is_called_with(self, data):
+            assert_that(self._validate_called_with).is_equal_to(data)
+
+
+
