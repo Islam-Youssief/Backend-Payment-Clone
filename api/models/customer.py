9a22a4f666b99d6e40dca7837e11528b1824806f
Exil <abdalrahmankay2019@gmail.com>
2026-07-22 13:37:15 +0300

    fixes

--- api/models/customer.py
+++ api/models/customer.py
@@ -2,11 +2,11 @@ from api.models import db,TimestampMixin
 
 
 
-class customer(db.Model, TimestampMixin):
+class Customer(db.Model, TimestampMixin):
     __tablename__ = "customers"
 
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     name = db.Column(db.String(255),nullable=False)
     email = db.Column(db.String(255),nullable=False,unique=True)
     
-    payments = db.relationship("payemntsHistory",back_populates="customer")
+    payments = db.relationship("PaymentsHistory",back_populates="customer")
