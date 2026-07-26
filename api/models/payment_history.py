ff66d7dbec2542858a5f67c1d8350b21e9093ba4
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-26 13:11:59 +0300

    fix: update Customer and PaymentHistory model relationships

--- api/models/payment_history.py
+++ api/models/payment_history.py
@@ -1,13 +1,17 @@
+import uuid
+
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
 from api.models import db, TimestampMixin
 
 
 class PaymentHistory(db.Model, TimestampMixin):
     
     __tablename__ = "payment_history"
-    id= db.Column(db.Integer, primary_key=True, autoincrement=True)
-    customer_id= db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
-    checkout_payment_id= db.Column(db.String(255), nullable=False, unique=True)
+    id= db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid7, nullable=False)
+    customer_id= db.Column(UUID(as_uuid=True), db.ForeignKey("customers.id"), nullable=False)
+    idempotency_key= db.Column(UUID(as_uuid=True), nullable=False, unique=True)
+    checkout_payment_id= db.Column(UUID(as_uuid=True), nullable=False, unique=True)
     amount= db.Column(db.Float, nullable=False)
     currency= db.Column(db.String(10), nullable=False)
     status= db.Column(db.String(50), nullable=False)
