c87562abe49e874e5516c96b6f6f143f620b5e9b
Exil <abdalrahmankay2019@gmail.com>
2026-07-27 13:40:37 +0300

    set customer foreign key on delete set null

--- api/models/paymentsHistory.py
+++ api/models/paymentsHistory.py
@@ -5,7 +5,7 @@ class PaymentsHistory(db.Model,TimestampMixin):
     __tablename__ = "payments_history"
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     transaction_id = db.Column(db.String(255),nullable=False,unique=True)
-    customer_id = db.Column(db.Integer,db.ForeignKey("customers.id"),nullable=False)
+    customer_id = db.Column(db.Integer,db.ForeignKey("customers.id",ondelete="SET NULL"),nullable=True)
     amount = db.Column(db.Numeric(10,2),nullable=False)
     currency = db.Column(db.String(50),nullable=False)
     provider = db.Column(db.String(50),nullable=False)
