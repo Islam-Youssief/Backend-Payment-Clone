67ad5c0c45a003084376e6579111f2993dabda3c
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-08-02 11:59:50 +0300

    chore: update requests count

--- api/services/rate_limit_service.py
+++ api/services/rate_limit_service.py
@@ -6,7 +6,7 @@ class RateLimitService:
 
         self.throttle = Throttled(
             using = RateLimiterType.FIXED_WINDOW.value,
-            quota = rate_limiter.per_sec(1),
+            quota = rate_limiter.per_min(10),
         )
     
     def check(self, key):
