004a25db0a9173033e2fb47ac1700dfa5b576666
Exil <abdalrahmankay2019@gmail.com>
2026-08-02 04:06:07 +0300

    Add idempotency + update

--- api/services/payment_history.py
+++ api/services/payment_history.py
@@ -1,4 +1,4 @@
-from api.models import PaymentsHistory
+from api.models import PaymentsHistory,db
 
 class PaymentHistoryService:
 
@@ -13,3 +13,14 @@ class PaymentHistoryService:
     @staticmethod
     def get_by_customer_id(customer_id):
         return PaymentsHistory.query.filter_by(customer_id=customer_id).all()
+
+    @staticmethod
+    def update_status_by_transaction_id(transaction_id,status,failure_reason=None):
+        payment = PaymentsHistory.query.filter_by(transaction_id=transaction_id).first()
+        if payment:
+            payment.status = status
+            if failure_reason:
+                payment.failure_reason = failure_reason
+                db.session.commit()
+        return payment
+    
