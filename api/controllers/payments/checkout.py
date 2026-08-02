c6a2c5e3dcc39d26de64696681649c498ae19d9a
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-08-02 11:52:44 +0300

    feat: add idempotency key

--- api/controllers/payments/checkout.py
+++ api/controllers/payments/checkout.py
@@ -18,11 +18,16 @@ class CheckoutController:
     @property
     def _body(self):
         return self._flask_request.json
+    
+    @property
+    def _idempotency_key(self):
+        return self._flask_request.headers.get("Idempotency-Key")
+    
 
     def pay(self):
         try:
             self._validator.validate(self._body)
-            invoice = self._handler.process_payment(data=self._body)
+            invoice = self._handler.process_payment(data=self._body , idempotency_key=self._idempotency_key)
             return (
                 _CheckoutSerializer(invoice).serialize(self._flask_request.path),
                 invoice.http_status
@@ -42,8 +47,8 @@ class _CheckoutHandler:
         self._config = config
         self._client = test_client or checkout_service.CheckoutClient(self._config.env)
 
-    def process_payment(self, data):
-        response = self._client.pay_with_card(data=data)
+    def process_payment(self, data, idempotency_key):
+        response = self._client.pay_with_card(data=data, idempotency_key=idempotency_key)
         invoice = sjson.JsonObject(response.json())
         if invoice.status == 'success':
             invoice.http_status = http.HTTPStatus.CREATED
--- api/services/payment/checkout.py
+++ api/services/payment/checkout.py
@@ -14,10 +14,11 @@ class CheckoutClient:
         self._env = env 
         self._request_sender = test_request_sender or requests_service.get_service(env)
         self._user_data_validator= user_data_validator or UserDataValidator()
-        self._rate_limiter = rate_limiter
+        self._rate_limiter = rate_limiter or rate_limiter.RateLimitService()
         
     
-    def pay_with_card(self, data):
+    def pay_with_card(self, data , idempotency_key):
+        self._idempotency_key = idempotency_key
         self._user_data_validator.validate_payment_data(data)
         self._rate_limiter.check(configuration.PaymentsConfig().checkout_secret_key)
         response = self._request_sender.request(method='POST', url=f'{configuration.PaymentsConfig().checkout_prefix_code}.{CHECKOUT_PAYMENT_URL}', json=data, headers=self._headers)
@@ -27,7 +28,8 @@ class CheckoutClient:
     def _headers(self):
         return{
             'Content-Type':'application/json',
-            'Authorization': f"Bearer {configuration.PaymentsConfig().checkout_secret_key}"
+            'Authorization': f"Bearer {configuration.PaymentsConfig().checkout_secret_key}",
+            "Cko-Idempotency-Key": self._idempotency_key,
         }
 
 class UserDataValidator:
